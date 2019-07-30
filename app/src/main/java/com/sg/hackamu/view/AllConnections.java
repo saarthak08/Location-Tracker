e60f0c71823e3d1f11f265e0827f46289df8a295
Saarthak <saarthakgupta08@gmail.com>
2019-07-30 09:55:53 +0530

    Added Search Layout

---
+++ app/src/main/java/com/sg/hackamu/view/AllConnections.java
@@ -0,0 +1,14 @@
+package com.sg.hackamu.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllConnections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_connections);
+    }
+}
